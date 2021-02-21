9ea234bf91acddbc20a321bb83e2723e468e706d
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-21 11:38:55 -0500

    started testing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,25 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const db = require('../data/dbConfig')
+const server = require('./server');
+const request = require('supertest');
+
+beforeAll(async ()=>{
+  await db.migrate.rollback();
+  await db.migrate.latest();
 })
+
+beforeEach(async ()=>{
+  await db('users').truncate()
+})
+
+afterAll(async ()=>{
+  await db.destroy()
+})
+
+describe('server.js',()=>{
+  test('we are in the testing env', ()=>{
+      expect(process.env.DB_ENV).toBe('development')
+  })
+})
+
+
